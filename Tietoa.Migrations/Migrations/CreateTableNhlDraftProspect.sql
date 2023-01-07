83b6cfaee4ab75b065de8942135e7e39c9c5a5ac
StuartDewing <Stuart@Dewing>
2023-01-07 10:49:17 +0000

    Connect Database Table Select from NhlDraftProspect

---
+++ Tietoa.Migrations/Migrations/CreateTableNhlDraftProspect.sql
@@ -0,0 +1,34 @@
+﻿/* To prevent any potential data loss issues, you should review this script in detail before running it outside the context of the database designer.*/
+BEGIN TRANSACTION
+SET QUOTED_IDENTIFIER ON
+SET ARITHABORT ON
+SET NUMERIC_ROUNDABORT OFF
+SET CONCAT_NULL_YIELDS_NULL ON
+SET ANSI_NULLS ON
+SET ANSI_PADDING ON
+SET ANSI_WARNINGS ON
+COMMIT
+BEGIN TRANSACTION
+GO
+CREATE TABLE dbo.NhlDraftProspect
+	(
+	id int NOT NULL IDENTITY (1, 1),
+	Round int NULL,
+	Pick int NULL,
+	Team varchar(50) NULL,
+	FullName nvarchar(255) NULL,
+	ProspectId int NULL
+	)  ON [PRIMARY]
+GO
+ALTER TABLE dbo.NhlDraftProspect ADD CONSTRAINT
+	PK_NhlDraftProspect PRIMARY KEY CLUSTERED 
+	(
+	id
+	) WITH( STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+
+GO
+ALTER TABLE dbo.NhlDraftProspect SET (LOCK_ESCALATION = TABLE)
+GO
+COMMIT
+
+providerName=System.Data.SqlClient
--- Tietoa/Controllers/Divisions/DivisionController.cs
+++ Tietoa/Controllers/Divisions/DivisionController.cs
@@ -1,6 +1,9 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
 using Services.NHL.NhlRequest;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
 using Tietoa.Domain.Models.Divisions;
 using Tietoa.Domain.Models.Divisions.JsonClasses;
 
@@ -12,16 +15,41 @@ namespace Tietoa.Controllers.Divisions
     {
         private readonly ILogger<DivisionController> _logger;
         private readonly INhlRequest _NhlRequest;
+        private readonly IConfiguration _configuration;
 
-        public DivisionController(ILogger<DivisionController> logger, INhlRequest nhlRequest)
+        public DivisionController(ILogger<DivisionController> logger, INhlRequest nhlRequest, IConfiguration configuration)
         {
             _logger = logger;
             _NhlRequest = nhlRequest;
+            _configuration = configuration;
         }
+
+        //TODO: Create Service
+        private DataTable GetData()
+        {
+            var config = _configuration.GetConnectionString("TietoaConnectionString");
+
+            SqlConnection conn = new SqlConnection(config);
+            conn.Open();
+            string query = "SELECT * FROM [NhlDraftProspect]";
+            SqlCommand cmd = new SqlCommand(query, conn);
+
+            DataTable dt = new DataTable();
+            dt.Load(cmd.ExecuteReader());
+            conn.Close();
+            return dt;
+        }
+
+
 
         [HttpGet]
         public async Task<IActionResult> Get()
         {
+
+            //TODO: Create Service
+            //GetData();
+
+
           
             var url = $"https://statsapi.web.nhl.com/api/v1/divisions";
             var response = await _NhlRequest.NHLGetResponse(url);
